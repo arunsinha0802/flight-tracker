a15814a0628f267271f095bfb0072801eec18a88
arunsinha0802 <arunsinha0802@gmail.com>
2026-03-15 17:56:36 +0000

    adding some details to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Location settings
+LOCATION_LAT = 51.54880905151367      # your latitude
+LOCATION_LON = -0.013320274651050568      # your longitude
+SEARCH_RADIUS_KM = 1.5       # how far around you to look for flights
+
+# Display settings
+#PANEL_WIDTH = 
+#PANEL_HEIGHT = 
+
+# Timing settings
+FLIGHT_POLL_INTERVAL = 20    # seconds between FlightRadar24 requests
+TEMPERATURE_POLL_INTERVAL = 300  # seconds between temperature requests (5 mins)
